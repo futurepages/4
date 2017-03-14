9305db461d3be6b4887dba2d0d15ef09a8cbf791
Leandro <iileandro@gmail.com>
2017-03-14 02:22:54 -0300

    bugfix: commas

--- src/org/futurepages/core/persistence/HQLProvider.java
+++ src/org/futurepages/core/persistence/HQLProvider.java
@@ -279,7 +279,9 @@ public class HQLProvider implements HQLable {
 			} else if (fields.length > 1) {
 				StringBuilder sb = new StringBuilder();
 				for (Object field : fields) {
-					sb.append(field.toString()).append(",");
+					if(!Is.empty(field)){
+						sb.append(field.toString()).append(",");
+					}
 				}
 				String result = sb.toString();
 				return result.substring(0, result.length() - 1);
