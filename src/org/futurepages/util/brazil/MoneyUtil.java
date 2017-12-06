4c352ece6d3254bd0fcca019cfd6b96656a5d19b
Leandro Santana Pereira <iileandro@gmail.com>
2017-12-06 17:20:40 -0300

    CDL Avançando

--- src/org/futurepages/util/brazil/MoneyUtil.java
+++ src/org/futurepages/util/brazil/MoneyUtil.java
@@ -56,7 +56,7 @@ public class MoneyUtil {
 
 
 	public static int intFormat(BigDecimal value){
-		return  Integer.valueOf(moneyFormat(value).replaceFirst(",",""));
+		return  Integer.valueOf(moneyFormat(value.setScale(2,BigDecimal.ROUND_HALF_UP)).replaceAll("\\.","").replaceFirst(",",""));
 	}
 
 
