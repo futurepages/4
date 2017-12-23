15a6a0ff5eb8963e1bf668f451197e4925aa3c61
Leandro Santana Pereira <iileandro@gmail.com>
2017-12-23 20:39:57 -0300

    Quartz now with another schedule executors.

--- src/org/futurepages/core/quartz/QuartzManager.java
+++ src/org/futurepages/core/quartz/QuartzManager.java
@@ -2,7 +2,11 @@ package org.futurepages.core.quartz;
 
 import java.io.File;
 import java.text.ParseException;
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+
 import org.futurepages.exceptions.NotModuleException;
 
 import org.quartz.Scheduler;
@@ -17,12 +21,22 @@ import org.quartz.impl.StdSchedulerFactory;
 public class QuartzManager {
 
 	private static SchedulerFactory schedulerFactory;
-	
+
+	private static List<ScheduledExecutorService> anotherScheduleds;
+
 	public static void initialize(File[] modules) throws SchedulerException, ClassNotFoundException, ParseException, NotModuleException {
 		schedulerFactory = new StdSchedulerFactory();
 		(new QuartzJobsRegister(modules)).register();
 		schedulerFactory.getScheduler().start();
 	}
+
+	public static void addOther(ScheduledExecutorService another){
+		if(anotherScheduleds ==null){
+			anotherScheduleds = new ArrayList<>();
+		}
+		anotherScheduleds.add(another);
+	}
+
 	
 	public static SchedulerFactory getSchedulerFactory(){
 		return schedulerFactory;
@@ -31,8 +45,16 @@ public class QuartzManager {
 	public static void shutdown() throws SchedulerException {
 		System.out.println("Killing Quartz's Jobs...");
 		Collection<Scheduler> schedulers = schedulerFactory.getAllSchedulers();
-		for(Scheduler scheduler : schedulers){
-			scheduler.shutdown();
+		if(schedulers!=null){
+			for(Scheduler scheduler : schedulers){
+				scheduler.shutdown();
+			}
+		}
+		if(anotherScheduleds!=null){
+			System.out.println("Killing Quartz's Another schedule Executors ...");
+			for(ScheduledExecutorService another : anotherScheduleds){
+				another.shutdown();
+			}
 		}
 		System.out.println("Quartz's Jobs successful killed.");
 	}
