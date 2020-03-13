cace4cc8d441f100f4bb06c6f621856bd194851e
Cristalino Jr <cristalino.jr@gmail.com>
2020-03-13 19:16:25 -0300

    PhoneUtil.NumberFormat.INTERNATIONAL on PhoneFormatter

--- src/org/futurepages/formatters/PhoneFormatter.java
+++ src/org/futurepages/formatters/PhoneFormatter.java
@@ -1,7 +1,9 @@
 package org.futurepages.formatters;
 
 import org.futurepages.core.formatter.AbstractFormatter;
+import org.futurepages.util.PhoneUtil;
 import org.futurepages.util.The;
+import org.futurepages.util.brazil.BrazilPhoneUtil;
 
 import java.util.Locale;
 
@@ -13,23 +15,6 @@ import java.util.Locale;
 public class PhoneFormatter extends AbstractFormatter<String> {
     
     public String format(String value, Locale locale) {
-        // +5511957050704  --> +55 (11) 9 5705-0704
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < value.length();i++){
-            if(i==3){
-               sb.append(" (");
-            }
-            sb.append(value.charAt(i));
-            if(i==4){
-                sb.append(") ");
-            }
-            if(i==5){
-                sb.append(" ");
-            }
-            if(i==9){
-                sb.append("-");
-            }
-        }
-        return sb.toString();
+        return BrazilPhoneUtil.normalize(value, PhoneUtil.NumberFormat.INTERNATIONAL);
     }
 }
