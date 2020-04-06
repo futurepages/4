cdfec0e3e191a3dd97a567f9e6da62d55bc2b948
Leandro Santana <iileandro@gmail.com>
2020-04-06 19:16:08 -0300

    GlobalActionsManager now has filter

--- src/org/futurepages/jersey/core/GlobalActionsManager.java
+++ src/org/futurepages/jersey/core/GlobalActionsManager.java
@@ -2,10 +2,15 @@ package org.futurepages.jersey.core;
 
 import org.futurepages.jersey.actions.JerseyAction;
 
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.FeatureContext;
 
-public class GlobalActionsManager {
+public class GlobalActionsManager implements Filter{
 
 	protected static JerseyAction action(){
 		return JerseyAction.invoked();
@@ -16,4 +21,13 @@ public class GlobalActionsManager {
 	public void app(ResourceInfo resourceInfo, FeatureContext context){}
 
 	public void sse(ResourceInfo resourceInfo, FeatureContext context){}
+
+	@Override
+	public void init(FilterConfig filterConfig) {}
+
+	@Override
+	public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {}
+
+	@Override
+	public void destroy() {}
 }
