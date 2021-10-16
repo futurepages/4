ca72799bc25a9ec30b6db8063b941ba332905287
Ranieri <jose.ranieri@yahoo.com.br>
2021-10-16 08:05:37 -0300

    New methods for AssertUtils

--- test/util/AssertUtils.java
+++ test/util/AssertUtils.java
@@ -8,9 +8,37 @@ import java.util.List;
 
 public class AssertUtils extends Assert{
 
+	/**
+	 * Method for compare msg
+	 * @param msg
+	 * @param results
+	 */
 	public static void assertTrues(String[] msg, boolean... results) {
 		boolean[] expectArray = new boolean[results.length];
 		Arrays.fill(expectArray, true);
 		Assert.assertEquals(The.implodedArray(msg,",",null), Arrays.toString(expectArray), Arrays.toString(results));
 	}
+
+	/**
+	 * Methods for compare lists
+	 * @param expected
+	 * @param actual
+	 */
+	public static void assertListEquals(List<String> expected, List<String> actual) {
+		for (int i = 0; i < actual.size() - 1; i++) {
+			Assert.assertEquals(expected.get(i), actual.get(i));
+		}
+	}
+
+	/**
+	 * Method for template object
+	 * @param template
+	 * @param params
+	 * @return
+	 */
+	public String expected(Object template, Object... params) {
+		return MessageFormat.format(template.toString(),params);
+	}
+
+
 }
