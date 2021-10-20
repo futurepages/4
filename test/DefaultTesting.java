32a7e2be9ba8b9ac97feeb631c4f8a99a6c0241c
Ranieri <jose.ranieri@yahoo.com.br>
2021-10-20 18:25:05 -0300

    Standard class for rule routine

---
+++ test/DefaultTesting.java
@@ -0,0 +1,23 @@
+package org.futurepages.test;
+
+import framework.util.AssertUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Rule;
+
+
+public class DefaultTesting extends AssertUtils {
+
+	@Rule
+	public DefaultTestLogger logger = new DefaultTestLogger();
+	@Rule
+	public ConditionalIgnoreRule conditional = new ConditionalIgnoreRule();
+
+	@BeforeClass
+	public static void _beforeClass(){
+		if(DriverFactory.isDriverLoaded()){
+			DriverFactory.getDefaultWebDriver().manage().deleteAllCookies();
+		}
+	}
+
+}
