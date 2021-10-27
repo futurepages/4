3003dcee31a81fef315d1f2d5db0d8441224cd26
Joao A. Cunha <j.a.c.cunha@hotmail.com>
2021-10-27 15:54:07 -0300

    Serviço obter da URL

--- test/TestingService.java
+++ test/TestingService.java
@@ -1,20 +1,19 @@
 package org.futurepages.test;
 
-/**
- * Nesta classe há a chamada de um pacote que temos que verificar se esta na lib do futurepages4
- */
-
 import okhttp3.Response;
 import org.futurepages.util.HttpUtil;
 import org.futurepages.util.The;
 
 public class TestingService {
 
-	/**
-	 *
-	 * @param url
-	 * @return
-	 */
+	public static String getContentResFromPath(String... path){
+		return HttpUtil.getURLContent(DriverFactory.getPath(The.concat(path)));
+	}
+
+	public static Response getResponseFromPath(String... path){
+		return HttpUtil.getURLResponse(DriverFactory.getPath(The.concat(path)));
+	}
+
 	public static Response getResponseFromURL(String url){
 		return HttpUtil.getURLResponse(url);
 	}
